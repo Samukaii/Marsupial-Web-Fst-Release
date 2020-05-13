15f9e555b6336514c6b40d8d5c72ebbd231883bb
Samuel <mukinha.2016@gmail.com>
2020-05-12 21:28:26 -0300

    Criada a base da interface inicial do app

---
+++ src/config/styles/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  sideBar: '#4355FA',
+  background: '#eee',
+  alerts: '#93B0C8',
+  search: '#fff',
+};
+
+//Esse alerts é só aqule bloquinho que dizia "237 dias para o Enem"
--- src/layouts/home/index.js
+++ src/layouts/home/index.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import {Background, MenuNav, Container} from './styles';
+import {Background, Dashboard, SearchBar, ContentCard, HideShowMenu} from './styles/index';
+import {SideBar, CardItem, InputSearch, SearchButton} from './styles/index';
+import {FontAwesomeIcon as FaIcon} from '@fortawesome/react-fontawesome';
+import {faSearch, faBars} from '@fortawesome/free-solid-svg-icons';
+import React, {useEffect, useRef, useState} from 'react';
+import {Colors} from '../../config/styles';
+import NavMenus from './navigationMenus';
+import {TweenLite} from 'gsap';
 
 export default function Home() {
-  let menuActive = true;
-  function toggleMenu() {
-    if (menuActive) {
-      //document.getElementById('menuNav').style.left = '-30px';
-      document.getElementById('menuNav').style.width = '4%';
+  let card = useRef(null);
+  let navMenu = useRef(null);
+  let sideBar = useRef(null);
+  let labelItems = useRef(null);
+  let menuItems = useRef(null);
+
+  const [isShownMenu, setIsShownMenu] = useState(true);
+
+  useEffect(() => {
+    TweenLite.to(card, 2, {
+      x: 50,
+    });
+    labelItems.current = sideBar.getElementsByClassName('Label');
+    menuItems.current = sideBar.getElementsByClassName('MenuItem');
+  }, []);
+  function hideOrShowMenu() {
+    if (isShownMenu) {
+      setIsShownMenu(false);
+      TweenLite.to(navMenu, 0.6, {
+        x: -100,
+      });
+      TweenLite.to(labelItems.current, 0.3, {
+        opacity: 0,
+      });
+      TweenLite.to(menuItems.current, 0.6, {
+        x: -34,
+      });
     } else {
-      //document.getElementById('menuNav').style.left = '0px';
-      document.getElementById('menuNav').style.width = '20%';
+      setIsShownMenu(true);
+      TweenLite.to(navMenu, 0.6, {
+        x: 0,
+      });
+      TweenLite.to(labelItems.current, 0.6, {
+        opacity: 100,
+      });
+      TweenLite.to(menuItems.current, 0.6, {
+        x: 0,
+      });
     }
-    menuActive = !menuActive;
   }
+
   return (
     <Background>
-      <MenuNav id='menuNav'>
-        <button onClick={toggleMenu} style={{float: 'right'}}>
-          Fechar
-        </button>
-      </MenuNav>
-      <Container>Seja bem Vindo</Container>
+      <SideBar ref={ref => (sideBar = ref)}>
+        <NavMenus />
+      </SideBar>
+      <Dashboard ref={nav => (navMenu = nav)}>
+        <HideShowMenu onClick={hideOrShowMenu}>
+          <FaIcon icon={faBars} color={Colors.sideBar} size='2x' style={{width: 20}} />
+        </HideShowMenu>
+        <SearchBar>
+          <InputSearch placeholder='Pesquise aqui...' />
+          <SearchButton>
+            <FaIcon icon={faSearch} color={Colors.sideBar} />
+          </SearchButton>
+        </SearchBar>
+        <ContentCard ref={el => (card = el)}>
+          <CardItem> Linguagens e suas Tecnologias</CardItem>
+          <CardItem> Ciências Humanas</CardItem>
+        </ContentCard>
+        <ContentCard ref={el => (card = el)}>
+          <CardItem> Matemática</CardItem>
+          <CardItem> Redação</CardItem>
+        </ContentCard>
+      </Dashboard>
     </Background>
   );
 }
